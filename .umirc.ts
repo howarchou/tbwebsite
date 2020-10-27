c246bfade9947c4dfee255037b0dc85c89be88e1
panwei <1184734081@qq.com>
2020-10-27 21:14:15 +0800

    修改样式

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,10 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [{ path: '/', component: '@/pages/home/index' }],
+  routes: [
+    { path: '/', component: '@/pages/home/index' },
+    { path: '/teambuilding', component: '@/pages/teambuilding/index' },
+  ],
   devServer: {
     port: 6677,
   },
---
+++ src/pages/teambuilding/index.tsx
@@ -0,0 +1,8 @@
+/**
+ *  Created by pw on 2020/10/8 9:06 下午.
+ */
+import React from 'react';
+
+export default function() {
+  return <div>teambuilding</div>;
+}
