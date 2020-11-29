294285cc16b79b8e67068421004b9c14980ad05c
panwei <1184734081@qq.com>
2020-11-29 17:55:52 +0800

    logo

--- src/pages/home/Partners.tsx
+++ src/pages/home/Partners.tsx
@@ -5,15 +5,14 @@ import React from 'react';
 import './Partners.less';
 import { PartnersIF } from '@/types';
 
-const GROUPING_COUNT = 5;
+const GROUPING_COUNT = 6;
 
 interface Props {
-  logos: API.Home_Logos[];
+  logos: PartnersIF[];
 }
 
 export default function(props: Props) {
   const { logos = [] } = props;
-  const cards: PartnersIF[] = [];
 
   const groups = logos.reduce<PartnersIF[][]>((result, current, index) => {
     const remainder = Math.floor(index / GROUPING_COUNT);
@@ -23,21 +22,24 @@ export default function(props: Props) {
     return result;
   }, []);
 
+  const handleClick = (logo: PartnersIF) => {
+    const w: any = window.open('about:blank');
+    w.location.href = logo.link;
+  };
+
   return (
     <div className="partners-wrapper">
       <div className="title">合作伙伴</div>
-      {/*<img*/}
-      {/*  className="partners-logo"*/}
-      {/*  src={*/}
-      {/*    'https://imetadata.damaotuanjian.com/METADATA/AFBDE8D0-8437-7FDE-CA17-F055036CAC3F-s1_webp'*/}
-      {/*  }*/}
-      {/*/>*/}
       {groups.map((group, index) => {
         return (
           <div key={index} className="partner-group">
             {group.map((logo, index) => {
               return (
-                <div key={index} className="partners-logo">
+                <div
+                  key={index}
+                  className="partners-logo"
+                  onClick={() => handleClick(logo)}
+                >
                   <img className="logo-img" src={logo.cover} />
                 </div>
               );
