7c0f09bec2d8a58b75f08312f0f70a7318aeb09b
Aleutian Xie <aleutian.xie@cicisoft.cn>
2021-06-07 09:26:02 +0800

    update _agl

--- src/pages/home/Footer.tsx
+++ src/pages/home/Footer.tsx
@@ -19,7 +19,21 @@ export default function() {
     }
     saveOrders({ ...values }).then(res => {
       // alert('提交成功!');
-      window._agl && window._agl.push(['track', ['success', { t: 3 }]]);
+      window._agl = window._agl || [];
+      (function() {
+        _agl.push(['production', '_f7L2XwGXjyszb4d1e2oxPybgD']);
+        (function() {
+          var agl = document.createElement('script');
+          agl.type = 'text/javascript';
+          agl.async = true;
+          agl.src =
+            'https://fxgate.baidu.com/angelia/fcagl.js?production=_f7L2XwGXjyszb4d1e2oxPybgD';
+          var s = document.getElementsByTagName('script')[0];
+          // @ts-ignore
+          s.parentNode.insertBefore(agl, s);
+        })();
+      })();
+
       setTimeout(function() {
         handleSubmitSuccessOpen();
       }, 100);
